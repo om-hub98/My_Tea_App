9b7d689a5b0a5109baec8e343ce288cbbd45e8f7
Omraj Pradhan <omrajpradhan@gmail.com>
2024-01-28 01:07:14 +0530

    [UPD] home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import Layout from '../component/Layout'
 import Header from '../component/Header'
+import Banner from '../Tea_Images/Banner.avif'
+import '../styles/HomeStyle.css'
 
 const Home = () => {
   return (
-    <div>
-      <section>
-          <h1>My home page</h1>
-      </section>
+     <div className='home' style={{background:`url(${Banner})`}}>
+      <div className='homeContainer'>
+        <h1 style={{margin:0}}>Enjoy the Essance Of Tea Leaves</h1>
+        <p style={{margin:0}}>Tea is not just a beverage it's an emotion..</p>
+      </div>
     </div>
   )
 }
